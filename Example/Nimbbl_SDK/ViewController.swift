ccf863905c8da46d5d429b546905c484dd9b245e
BushraLogicLoop <bushra@logicloop.io>
2023-05-29 16:13:12 +0530

    initial commit

---
+++ Example/Nimbbl_SDK/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Nimbbl_SDK
+//
+//  Created by BushraLogicLoop on 05/29/2023.
+//  Copyright (c) 2023 BushraLogicLoop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
